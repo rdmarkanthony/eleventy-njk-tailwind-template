b3fd54ba21ede7e1474e571303933ecf8317d7cd
rdmarkanthony <markrecto@ripplewerkz.com>
2026-02-20 13:35:07 +0800

    update: prettier; html output adjustment

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,24 +1,38 @@
+import prettier from "prettier";
+
 export default function (eleventyConfig) {
-  // update Nunjucks environment options
-  eleventyConfig.setNunjucksEnvironmentOptions({
-    trimBlocks: true,
-    lstripBlocks: true,
-  });
+    // update Nunjucks environment options
+    eleventyConfig.setNunjucksEnvironmentOptions({
+        trimBlocks: true,
+        lstripBlocks: true,
+    });
 
-  // watch css change
-  eleventyConfig.addWatchTarget("src/assets/css/");
+    // watch css change
+    eleventyConfig.addWatchTarget("src/assets/css/");
 
-  // port the assets
-  eleventyConfig.addPassthroughCopy("src/assets/img");
-  // eleventyConfig.addPassthroughCopy("src/assets/video");
-  // eleventyConfig.addPassthroughCopy("src/assets/data");
+    // port the assets
+    eleventyConfig.addPassthroughCopy("src/assets/img");
+    // eleventyConfig.addPassthroughCopy("src/assets/video");
+    // eleventyConfig.addPassthroughCopy("src/assets/data");
 
-  return {
-    markdownTemplateEngine: "njk",
-    dir: {
-      input: "src",
-      output: "public",
-      includes: "_includes",
-    },
-  };
+    // prettify HTML
+    eleventyConfig.addTransform("prettify-html", function (content, outputPath) {
+        if (outputPath && outputPath.endsWith(".html")) {
+            return prettier.format(content, {
+                parser: "html",
+                printWidth: 300,
+                tabWidth: 4,
+            });
+        }
+        return content;
+    });
+
+    return {
+        markdownTemplateEngine: "njk",
+        dir: {
+            input: "src",
+            output: "public",
+            includes: "_includes",
+        },
+    };
 }
