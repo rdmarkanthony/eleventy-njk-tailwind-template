c8117f2a546b59ff00549c1eadba688c8ff7d842
rdmarkanthony <markrecto@ripplewerkz.com>
2026-02-20 13:23:06 +0800

    update: css; tailwind needs vscode setting

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,18 +1,20 @@
 export default function (eleventyConfig) {
-  // Update Nunjucks environment options
+  // update Nunjucks environment options
   eleventyConfig.setNunjucksEnvironmentOptions({
     trimBlocks: true,
     lstripBlocks: true,
   });
 
+  // watch css change
+  eleventyConfig.addWatchTarget("src/assets/css/");
+
+  // port the assets
   eleventyConfig.addPassthroughCopy("src/assets/img");
   // eleventyConfig.addPassthroughCopy("src/assets/video");
   // eleventyConfig.addPassthroughCopy("src/assets/data");
 
   return {
     markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-    templateFormats: ["njk", "html"],
     dir: {
       input: "src",
       output: "public",
